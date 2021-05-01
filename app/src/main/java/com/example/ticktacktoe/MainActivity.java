9892338f5f4750a3fa97da52aff93dea7c7d5cd2
binarypirate <aliadi6211@gmail.com>
2021-05-01 10:44:40 +0500

    add listeners on each button

--- app/src/main/java/com/example/ticktacktoe/MainActivity.java
+++ app/src/main/java/com/example/ticktacktoe/MainActivity.java
@@ -4,11 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.ticktacktoe.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.topLeft.setOnClickListener(v -> {
+
+        });
+        mBinding.topMid.setOnClickListener(v -> {
+
+        });
+        mBinding.topRight.setOnClickListener(v -> {
+
+        });
+        mBinding.midLeft.setOnClickListener(v -> {
+
+        });
+        mBinding.midMid.setOnClickListener(v -> {
+
+        });
+        mBinding.midRight.setOnClickListener(v -> {
+
+        });
+        mBinding.bottomLeft.setOnClickListener(v -> {
+
+        });
+        mBinding.bottomMid.setOnClickListener(v -> {
+
+        });
+        mBinding.bottomRight.setOnClickListener(v -> {
+
+        });
     }
 }
